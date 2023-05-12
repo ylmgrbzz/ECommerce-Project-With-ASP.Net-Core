52c6f27f14bdf1208e138c9d2f8641d293024ebb
YALIM GÜRBÜZ <yalim1999@hotmail.com>
2023-05-12 14:55:43 +0300

    FinalProject

--- DataAccess/Concrete/InMemory/InMemoryProductDal.cs
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -10,6 +10,21 @@ namespace DataAccess.Concrete.InMemory
 {
     public class InMemoryProductDal : IProductDal
     {
+        List<Product> _products; // Global değişkenler alt çizgi ile başlar.
+
+        public InMemoryProductDal()
+        {
+            _products = new List<Product>
+            {
+                new Product{CategoryId=1,ProductId=1,ProductName="Bardak",UnitPrice=15,UnitsInStock=15},
+                new Product{CategoryId=2,ProductId=2,ProductName="Kamera",UnitPrice=500,UnitsInStock=3},
+                new Product{CategoryId=3,ProductId=3,ProductName="Telefon",UnitPrice=1500,UnitsInStock=2},
+                new Product{CategoryId=4,ProductId=4,ProductName="Klavye",UnitPrice=150,UnitsInStock=65},
+                new Product{CategoryId=5,ProductId=5,ProductName="Fare",UnitPrice=85,UnitsInStock=1}
+
+        };
+        }
+
         public void Add(Product product)
         {
             throw new NotImplementedException();
