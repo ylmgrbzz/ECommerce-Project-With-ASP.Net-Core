a8d046a358b343c35cc613920e0b72f42ce6f715
YALIM GÜRBÜZ <yalim1999@hotmail.com>
2023-05-12 14:29:02 +0300

    Proje dosyası ekle.

---
+++ Entities/Concrete/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public int CategoryId { get; set; }
+        public string ProductName { get; set; } // ProductName isimli bir property tanımladık.
+        public decimal UnitPrice { get; set; } // UnitPrice isimli bir property tanımladık.
+        public short UnitsInStock { get; set; } // UnitsInStock isimli bir property tanımladık.
+    }
+}
