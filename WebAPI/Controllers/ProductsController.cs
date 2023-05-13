129b3d7c7c30da7094450906cc1249df54039da3
YALIM GÜRBÜZ <yalim1999@hotmail.com>
2023-05-13 23:24:32 +0300

    WebAPI

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Merhaba";
+        }
+        //private IProductService _productService;
+        //public ProductsController(IProductService productService)
+        //{
+        //    _productService = productService;
+        //}
+        //public IActionResult Get()
+        //{
+        //    var result = _productService.GetAll();
+        //    if (result.Success)
+        //    {
+        //        return Ok(result);
+        //    }
+        //    return BadRequest(result);
+        //}
+        //public IActionResult Get(int id)
+        //{
+        //    var result = _productService.GetById(id);
+        //    if (result.Success)
+        //    {
+        //        return Ok(result);
+        //    }
+        //    return BadRequest(result);
+        //}
+        //public IActionResult Add(Product product)
+        //{
+        //    var result = _productService.Add(product);
+        //    if (result.Success)
+        //    {
+        //        return Ok(result);
+        //    }
+        //    return BadRequest(result);
+        //}
+        //public IActionResult Update(Product product)
+        //{
+        //    var result = _productService.Update(product);
+        //    if (result.Success)
+        //    {
+        //        return Ok(result);
+        //    }
+        //    return BadRequest(result);
+        //}
+        //public IActionResult Delete(Product product)
+        //{
+        //    var result = _productService.Delete(product);
+        //    if (result.Success)
+        //    {
+        //        return Ok(result);
+        //    }
+        //    return BadRequest(result);
+        //} 
+
+    }
+}
