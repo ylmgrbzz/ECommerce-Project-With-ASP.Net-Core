5403bd2a06143e6fbed7979e70486b8f1168787e
YALIM GÜRBÜZ <yalim1999@hotmail.com>
2023-05-13 11:59:42 +0300

    FinalProject

--- Business/Abstract/IProductService.cs
+++ Business/Abstract/IProductService.cs
@@ -14,6 +14,10 @@ namespace Business.Abstract
         //void Update(Product product);
         //void Delete(Product product);
         List<Product> GetAll();
+        List<Product> GetAllByCategoryId(int id);
+        List<Product> GetByUnitPrice(decimal min, decimal max);
+
+
         //Product GetById(int productId);
     }
 }
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -11,13 +11,20 @@ using System.Threading.Tasks;
 
 namespace ConsoleUI
 {
+    // SOLID
+    // Open Closed Principle
+
     class Program
     {
         static void Main(string[] args)
         {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var product in productManager.GetAll())
+            foreach (var product in productManager.GetAllByCategoryId(2))
+            {
+                Console.WriteLine(product.ProductName);
+            }
+            foreach (var product in productManager.GetByUnitPrice(50, 100))
             {
                 Console.WriteLine(product.ProductName);
             }
