22beebac1b32459bd0d351e83ebf8ee3fbcad494
YALIM GÜRBÜZ <yalim1999@hotmail.com>
2023-05-14 14:52:23 +0300

    FinalProject

--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -1,6 +1,10 @@
-﻿using Entities.Concrete;
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
 
 namespace WebAPI.Controllers
 {
@@ -9,15 +13,26 @@ namespace WebAPI.Controllers
     public class ProductsController : ControllerBase
     {
         //private const string ResponseMessage = "Hello from ProductsController";
+        IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
 
         [HttpGet]
         public List<Product> Get()
+
         {
-            return new List<Product>
-           {
-                    new Product { ProductId = 1, ProductName = "Product 1" },
-                    new Product { ProductId = 2, ProductName = "Product 2" }
-                };
+
+            // return new List<Product>
+            //{
+            //         new Product { ProductId = 1, ProductName = "Product 1" },
+            //         new Product { ProductId = 2, ProductName = "Product 2" }
+            // };
+            //IProductService productService = new ProductManager(new EfProductDal());
+            var result = _productService.GetAll();
+            return result.Data;
         }
     }
 }
