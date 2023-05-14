a8fc5ea470073da24e68efc76320aff808b3be59
YALIM GÜRBÜZ <yalim1999@hotmail.com>
2023-05-14 14:27:35 +0300

    FinalProject

--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebAPI.Controllers
@@ -7,61 +8,16 @@ namespace WebAPI.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
-        [HttpGet]
-        public string Get()
-        {
-            return "Merhaba";
-        }
-        //private IProductService _productService;
-        //public ProductsController(IProductService productService)
-        //{
-        //    _productService = productService;
-        //}
-        //public IActionResult Get()
-        //{
-        //    var result = _productService.GetAll();
-        //    if (result.Success)
-        //    {
-        //        return Ok(result);
-        //    }
-        //    return BadRequest(result);
-        //}
-        //public IActionResult Get(int id)
-        //{
-        //    var result = _productService.GetById(id);
-        //    if (result.Success)
-        //    {
-        //        return Ok(result);
-        //    }
-        //    return BadRequest(result);
-        //}
-        //public IActionResult Add(Product product)
-        //{
-        //    var result = _productService.Add(product);
-        //    if (result.Success)
-        //    {
-        //        return Ok(result);
-        //    }
-        //    return BadRequest(result);
-        //}
-        //public IActionResult Update(Product product)
-        //{
-        //    var result = _productService.Update(product);
-        //    if (result.Success)
-        //    {
-        //        return Ok(result);
-        //    }
-        //    return BadRequest(result);
-        //}
-        //public IActionResult Delete(Product product)
-        //{
-        //    var result = _productService.Delete(product);
-        //    if (result.Success)
-        //    {
-        //        return Ok(result);
-        //    }
-        //    return BadRequest(result);
-        //} 
+        //private const string ResponseMessage = "Hello from ProductsController";
 
+        [HttpGet]
+        public List<Product> Get()
+        {
+            return new List<Product>
+           {
+                    new Product { ProductId = 1, ProductName = "Product 1" },
+                    new Product { ProductId = 2, ProductName = "Product 2" }
+                };
+        }
     }
 }
