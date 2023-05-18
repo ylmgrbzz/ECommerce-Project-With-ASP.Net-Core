15af2963bc53a86c7b351a5517c79f2ae19afd90
YALIM GÜRBÜZ <yalim1999@hotmail.com>
2023-05-18 12:08:19 +0300

    E Commerce Project

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -4,6 +4,7 @@ using Business.Constants;
 using Business.ValidationRules.FluentValidation;
 using Core.Aspects.Autofac.Validation;
 using Core.CrossCuttingConcerns.Validation;
+using Core.Utilities.Business;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using DataAccess.Concrete.InMemory;
@@ -30,7 +31,6 @@ namespace Business.Concrete
         {
             _productDal = productDal;
             //_logger = logger;
-
         }
 
         [ValidationAspect(typeof(ProductValidator))]
@@ -40,22 +40,18 @@ namespace Business.Concrete
             //{
             //    return new ErrorResult(Messages.ProductNameInvalid);
             //}
-
             //ValidationTool.Validate(new ProductValidator(), product);
 
+            IResult result = BusinessRules.Run(CheckIfProductNameExists(product.ProductName),
+                                CheckIfProductCountOfCategoryCorrect(product.CategoryId));
 
-            if (CheckIfProductCountOfCategoryCorrect(product.CategoryId).Success)
+            if (result != null)
             {
-                if (CheckIfProductNameExists(product.ProductName).Success)
-                {
-                    _productDal.Add(product);
-                    return new SuccessResult(Messages.ProductAdded);
-                }
-                return new ErrorResult();
+                return result;
             }
 
-
-
+            _productDal.Add(product);
+            return new SuccessResult(Messages.ProductAdded);
         }
 
         public IDataResult<List<Product>> GetAll()
@@ -63,11 +59,8 @@ namespace Business.Concrete
             if (DateTime.Now.Hour == 22)
             {
                 return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
-
             }
-
             return new SuccessDataResult<List<Product>>(_productDal.GetAll(), Messages.ProductsListed);
-
         }
 
         public IDataResult<List<Product>> GetAllByCategoryId(int id)
@@ -79,7 +72,6 @@ namespace Business.Concrete
         {
             //return _productDal.Get(p => p.ProductId == productId);
             return new SuccessDataResult<Product>(_productDal.Get(p => p.ProductId == productId));
-
         }
 
         public IDataResult<List<Product>> GetByUnitPrice(decimal min, decimal max)
@@ -93,7 +85,6 @@ namespace Business.Concrete
             //{
             //    return new ErrorResult(Messages.MaintenanceTime);
             //}
-
             return new SuccessDataResult<List<ProductDetailDto>>(_productDal.GetProductDetails());
         }
 
@@ -107,7 +98,6 @@ namespace Business.Concrete
             {
                 return new ErrorResult(Messages.ProductCountOfCategoryError);
             }
-
             _productDal.Update(product);
             return new SuccessResult(Messages.ProductAdded);
         }
@@ -118,6 +108,16 @@ namespace Business.Concrete
             if (result >= 10)
             {
                 return new ErrorResult();
+            }
+            return new SuccessResult();
+        }
+
+        private IResult CheckIfProductNameExists(string productName)
+        {
+            var result = _productDal.GetAll(p => p.ProductName == productName).Any();
+            if (result)
+            {
+                return new ErrorResult(Messages.ProductNameAlreadyExists);
             }
             return new SuccessResult();
         }
