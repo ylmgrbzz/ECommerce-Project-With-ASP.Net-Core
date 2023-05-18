cb4591016afe97e8dcf412eb7c84dda5cfa6a09c
YALIM GÜRBÜZ <yalim1999@hotmail.com>
2023-05-18 15:53:59 +0300

    E Commerce Project

---
+++ Core/Utilities/Security/Hashing/HashingHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Security.Hashing
+{
+    public class HashingHelper
+    {
+        public static void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key; // Key is a random generated key
+                passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)); // ComputeHash() takes a byte array
+
+            }
+
+
+
+        }
+        public static void VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+        }
+    }
+}
